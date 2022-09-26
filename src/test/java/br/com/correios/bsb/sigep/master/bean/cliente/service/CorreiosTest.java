c5ce5c52146e102c2bd33acd668789e1fdec7ee6
Dickson Henrique Justino Daniel <ckrique@hotmail.com>
2022-09-26 19:29:48 -0300

    Ajuste do teste unitário.
    Ajuste do teste unitário.

--- src/test/java/br/com/correios/bsb/sigep/master/bean/cliente/service/CorreiosTest.java
+++ src/test/java/br/com/correios/bsb/sigep/master/bean/cliente/service/CorreiosTest.java
@@ -1,62 +1,36 @@
 package br.com.correios.bsb.sigep.master.bean.cliente.service;
 
-
 import java.rmi.RemoteException;
 
 import org.apache.axis2.AxisFault;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-import br.com.worldcupbet.soapworldcupbet.services.GetByPunterNameDocument;
-import br.com.worldcupbet.soapworldcupbet.services.GetByPunterNameDocument.GetByPunterName;
-import br.com.worldcupbet.soapworldcupbet.services.GetByPunterNameResponseDocument;
-import br.com.worldcupbet.soapworldcupbet.services.GetByPunterNameResponseDocument.GetByPunterNameResponse;
-import br.com.worldcupbet.soapworldcupbet.services.xsd.Bet;
-import br.com.worldcupbet.soapworldcupbet.ws.WorldCupBetServiceStub;
-import br.com.correios.bsb.sigep.master.bean.cliente.impl.ConsultaCEPImpl;
-import br.com.correios.bsb.sigep.master.bean.cliente.impl.ConsultaCEPDocumentImpl;
-import br.com.correios.bsb.sigep.master.bean.cliente.impl.ConsultaCEPResponseDocumentImpl;
-import br.com.correios.bsb.sigep.master.bean.cliente.impl.ConsultaCEPResponseImpl;
-
-
 import br.com.correios.bsb.sigep.master.bean.cliente.*;
 import br.com.correios.ws.*;
 
-
-
-
-
-
-
 public class CorreiosTest {
 
-	
 	@Test
 	public void testConsultaCep() {
-			
+		String cep = "21221220";
 		AtendeClienteServiceStub stub;
 		try {
 			stub = new AtendeClienteServiceStub();
-			
-			br.com.correios.bsb.sigep.master.bean.cliente.ConsultaCEPDocument consultaCEP30 = br.com.correios.bsb.sigep.master.bean.cliente.ConsultaCEPDocument.Factory.newInstance();
-			br.com.correios.bsb.sigep.master.bean.cliente.ConsultaCEP consultaCep = br.com.correios.bsb.sigep.master.bean.cliente.ConsultaCEP.Factory.newInstance();
-			consultaCep.setCep("21221220");
-			
-			
+
+			ConsultaCEPDocument consultaCEP30 = ConsultaCEPDocument.Factory.newInstance();
+			ConsultaCEP consultaCep = ConsultaCEP.Factory.newInstance();
+			consultaCep.setCep(cep);
+
 			consultaCEP30.setConsultaCEP(consultaCep);
-			
-			br.com.correios.bsb.sigep.master.bean.cliente.ConsultaCEPResponseDocument consultaCEPResponseDocument = stub.consultaCEP(consultaCEP30);
-			
-			br.com.correios.bsb.sigep.master.bean.cliente.ConsultaCEPResponse response = consultaCEPResponseDocument.getConsultaCEPResponse();
-			
+
+			ConsultaCEPResponseDocument consultaCEPResponseDocument = stub.consultaCEP(consultaCEP30);
+			ConsultaCEPResponse response = consultaCEPResponseDocument.getConsultaCEPResponse();
 			EnderecoERP endereco = response.getReturn();
 			
-			System.out.println(endereco.getCep());
-			System.out.println(endereco.getBairro());
-			
-			stub = stub;
-			
+			Assert.assertTrue(endereco.getCep().equals("21221220"));
+			Assert.assertNotNull(endereco.getBairro());
+
 		} catch (AxisFault e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -70,54 +44,7 @@ public class CorreiosTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		}
-	
-	
-	
-	@Test
-	public void testGetHighScoresForNickname() {
-		
-		WorldCupBetServiceStub stub = null;
-		try {
-			stub = new WorldCupBetServiceStub();
-		} catch (AxisFault e) {
-			e.printStackTrace();
-			Assert.fail();
-		}
 
-		Assert.assertNotNull(stub);
-
-		GetByPunterNameDocument requestDoc = GetByPunterNameDocument.Factory.newInstance();
-		
-		GetByPunterName request = GetByPunterName.Factory.newInstance();
-		
-		request.setPunterName("Dickson");
-		
-		requestDoc.setGetByPunterName(request);
-		
-		GetByPunterNameResponseDocument responseDoc = null;
-		try {
-			responseDoc = stub.getByPunterName(requestDoc);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-			Assert.fail();
-		}
-		
-		Assert.assertNotNull(responseDoc);
-		
-		GetByPunterNameResponse response = responseDoc.getGetByPunterNameResponse();
-		
-		Assert.assertNotNull(response);
-		
-		Bet result = response.getReturn();
-		
-		Assert.assertNotNull(result);
-		
-		Assert.assertTrue(result.getPunterName().equals("Dickson"));
 	}
-	
 
 }
